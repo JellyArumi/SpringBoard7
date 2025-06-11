52bf993a75f115bd3e55aa0187b8b7b754362d34
arumiiiiii <cocoahreum@gmail.com>
2025-06-11 12:42:32 +0900

    111

--- SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //@RequestMapping(value="/board/*")
 //=> 실행하는 주소가 /board/~시작하는 모든 주소를
 //   해당 컨트롤러가 처리하겠다
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/board/*")
@@ -16,7 +17,20 @@ public class BoardController {
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
 	// 기능을 정의
+	
+	//http://localhost:8088/controller/board/regist   ( x) 
+	//http://localhost:8088/board/regist  (o) 
+	// 글쓰기(정보 입력) / GET
+	@RequestMapping(value="/regist",method = RequestMethod.GET )
+	public void boardRegistGET() {
+	
+		logger.info("boardRegistGET()실행");
+	}
+	
+	// 글쓰기(정보 처리) / POST
 
+	
+	
 	
 	
 	
